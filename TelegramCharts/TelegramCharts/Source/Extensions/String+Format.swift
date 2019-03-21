a9b7e2c22bf03b22921e803c60111311b4066bd9
Ростовский Никита Александрович <Rostovskiy.N.A@sberbank.ru>
2019-03-21 17:57:10 +0300

    String format changed

--- TelegramCharts/TelegramCharts/Source/Extensions/String+Format.swift
+++ TelegramCharts/TelegramCharts/Source/Extensions/String+Format.swift
@@ -9,17 +9,38 @@
 import UIKit
 
 extension String {
-    
-    private static let suffix = ["", "k", "m", "b", "t", "p", "e"]
 
     init(number: Int) {
-        var index = 0
-        var value = number
-        while (value / 1000 >= 1) {
-            value /=  1000
-            index += 1
-        }
-        self = String(format: "%d%@", value, String.suffix[index])
+        let numFormatter = NumberFormatter()
+
+        typealias Abbrevation = (threshold:Double, divisor:Double, suffix:String)
+        let abbreviations:[Abbrevation] = [(0, 1, ""),
+                                           (1000.0, 1000.0, "k"),
+                                           (100_000.0, 1_000_000.0, "m"),
+                                           (100_000_000.0, 1_000_000_000.0, "b")]
+
+        let startValue = Double(abs(number))
+        let abbreviation: Abbrevation = {
+            var prevAbbreviation = abbreviations[0]
+            for tmpAbbreviation in abbreviations {
+                if (startValue < tmpAbbreviation.threshold) {
+                    break
+                }
+                prevAbbreviation = tmpAbbreviation
+            }
+            return prevAbbreviation
+        } ()
+
+        let value = Double(number) / abbreviation.divisor
+        numFormatter.positiveSuffix = abbreviation.suffix
+        numFormatter.negativeSuffix = abbreviation.suffix
+        numFormatter.allowsFloats = true
+        numFormatter.minimumIntegerDigits = 1
+        numFormatter.minimumFractionDigits = 0
+        numFormatter.maximumFractionDigits = 1
+        numFormatter.decimalSeparator = "."
+
+        self = numFormatter.string(from: NSNumber(floatLiteral: value)) ?? ""
     }
 
 }
